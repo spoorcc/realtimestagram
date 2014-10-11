bc71b5659bbec27b5ce1e00a5f3ce0553f4f4a22
ben <ben.spoor@gmail.com>
2014-10-11 22:45:21 +0200

    First sigmoid function works, needs cleanup

--- src/curve_adjust.vhd
+++ src/curve_adjust.vhd
@@ -16,37 +16,37 @@
 --! <!------------------------------------------------------------------------------>
 --! <!------------------------------------------------------------------------------>
 --! \class curve_adjust
---! \brief Applies a curve adjustment
+--! \brief applies a curve adjustment
 --!
 --! \dot
 --! digraph curve_adjust{
 --!  
---!  graph [rankdir=LR, splines=ortho, sep=5];
+--!  graph [rankdir=lr, splines=ortho, sep=5];
 --!  edge  [penwidth=2.2, arrowsize=.5]
 --!  node  [height=0.25, width=0.25, style=filled, fontname=sans]
 --!
---!  /* Single or multibit registers */
+--!  /* single or multibit registers */
 --!  subgraph registers{
 --!      node [fontcolor=white, fontname=serif, fillcolor=gray32, shape=box, headport=w, tailport=e]
---!      clk rst enable input_pixel output_pixel
+--!      clk rst enable pixel_i pixel_o
 --!  }
 --!
 --!  subgraph cluster_0 {
 --!
 --!      color=gray128;
---!      label=CURVE_ADJUST;
+--!      label=curve_adjust;
 --!      fontcolor=black;
 --!      fontname=sans;
 --!
---!      LUT  [label="LUT", height=2, shape=box, fillcolor=gray96, fontcolor=black, tailport=e]
+--!      lut  [label="lut", height=2, shape=box, fillcolor=gray96, fontcolor=black, tailport=e]
 --!      and0 [label="&", shape=circle, fillcolor=white, fontcolor=black, fixedsize=true]
 --!  }
 --!
 --!  clk -> and0
 --!  enable -> and0
 --!  rst -> and0 [arrowhead=odot, arrowsize=0.6]
---!  and0 -> LUT 
---!  input_pixel -> LUT -> output_pixel
+--!  and0 -> lut 
+--!  pixel_i -> lut -> pixel_o
 --!
 --!}
 --! \enddot
@@ -58,76 +58,86 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-
+use ieee.math_real.all;
 
 --============================================================================--
 --!
 --!
 --!
 --!
-ENTITY curve_adjust IS
-  GENERIC (
+entity curve_adjust is
+  generic (
     wordsize:             integer  --! input image wordsize in bits
   );
-  PORT (
+  port (
     clk:                  in std_logic;       --! completely clocked process
     rst:                  in std_logic;       --! asynchronous reset
     enable:               in std_logic;       --! enables block
-    input_pixel:          in std_logic_vector((wordsize-1) downto 0);       --! the input pixel
+    pixel_i:              in std_logic_vector((wordsize-1) downto 0);       --! the input pixel
 
-    output_pixel:         out std_logic_vector((wordsize-1) downto 0)       --! the output pixel
+    pixel_o:              out std_logic_vector((wordsize-1) downto 0)       --! the output pixel
   );
-END ENTITY;
+end entity;
 
 --============================================================================--
 
-ARCHITECTURE curve_adjust_stub OF curve_adjust IS
+architecture curve_adjust_stub of curve_adjust is
 
-  -- signal declarations
-  SIGNAL the_signal:       std_logic_vector((wordsize-1) downto 0);
+    --! \brief array of std_logic_vectors
+    type array_pixel is array (natural range <>) of std_logic_vector(wordsize-1 downto 0);
 
---! \brief Array of integers
-type array_integer is array (natural range <>) of integer;
+    --! \fn create_lookup_table
+    --! \brief creates a lookup table using some predefined formula 
+    --! \description
+    --!  calculates every value and after that returns and integer array 
+    --! \param[in] size integer number of elements in returned array
+    function create_lookup_table(size: integer) return array_pixel is
+        variable return_value: array_pixel(0 to size - 1); --! Filled Look up table
+        variable exponent: real := 0.0;
+        variable calc_val: real := 0.0;
+        constant max_val: real := 255.0;
+        constant c: real := 15.0;
+    begin
+        for i in return_value'range loop
+            exponent :=  (c/max_val)*(real(i) - max_val * 0.5 );
+            calc_val := ceil(max_val / (real(1) + exp(-exponent)));
+            assert(1 = 0) report integer'image(integer(calc_val));
+            assert(integer(calc_val) <= integer(max_val)) report "LUT filled with invalid value: " & integer'image(integer(calc_val)) severity failure;
+            assert(integer(calc_val) > 0) report "LUT filled with invalid value" severity failure;
+            return_value(i) := std_logic_vector(to_unsigned(integer(calc_val), wordsize));
+        end loop;
+        return return_value;
+    end function create_lookup_table;
+    
+    -- signal declarations
+    signal lut_value_s:       std_logic_vector((wordsize-1) downto 0);
 
---! 
---! \fn create_lookup_table
---! \brief Creates a lookup table using some predefined formula 
---! \description
---!  Calculates every value and after that returns and integer array 
---! \param[in] size Integer number of elements in returned array
+    --! Look up table size  
+    constant lut_size_c: natural := 2**wordsize;
 
-function create_lookup_table(size: integer) return array_integer is
-    variable return_value: array_integer(0 to size - 1);
+    --! Generated lookup table
+    constant lut_contents: array_pixel(0 to (lut_size_c-1)) := create_lookup_table(lut_size_c); 
 begin
-    for i in return_value'range loop
-        array_integer(i) := INTEGER(i);
-    end loop;
-end function create_lookup_table;
+    
+    --! \brief clocked process that outputs the curve on each rising edge if enable is true 
+    --! \param[in] clk clock
+    --! \param[in] rst asynchronous reset
+    curve_adjustment : process(clk, rst)
+    begin
+        if rst = '1' then
+            lut_value_s  <= (others => '0');
 
-constant LUT_SIZE: natural := 2**wordsize; --! Size of the lookup-tables
-constant LUT_CONTENTS: array_integer(0 to (LUT_SIZE-1)) := create_lookup_table(LUT_SIZE); --! generated lookup table
+        elsif rising_edge(clk) then
 
-BEGIN
+            if enable = '1' then
+                lut_value_s <= lut_contents(to_integer(unsigned(pixel_i)));
+                pixel_o <= lut_value_s;
+            else
+                pixel_o <= (others => '0');
+            end if; -- end if enable = '1'
 
-  --! \brief Clocked process that outputs the curve on each rising edge if enable is true 
-  --! \param[in] clk clock
-  --! \param[in] rst asynchronous reset
-  curve_adjustment : PROCESS(clk, rst)
-  BEGIN
-    IF rst = '1' THEN
-      the_signal  <= (others => '0');
-
-    ELSIF RISING_EDGE(clk) THEN
-
-      IF enable = '1' THEN
-
-        the_signal <= STD_LOGIC_VECTOR(TO_UNSIGNED(LUT_CONTENTS(INTEGER(input_pixel)), wordsize-1));
-        output_pixel <= the_signal;
-
-      END IF; -- end if enable = '1'
-
-    END IF; -- end if rst = '1'
-  END PROCESS;
-END ARCHITECTURE;
+        end if; -- end if rst = '1'
+    end process;
+end architecture;
 
 --============================================================================--
--- src/curve_adjust_tb.vhd
+++ src/curve_adjust_tb.vhd
@@ -13,195 +13,150 @@
 --   You should have received a copy of the GNU General Public License
 --   along with Realtimestagram.  If not, see <http://www.gnu.org/licenses/>.
 
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use std.textio.all;
 
-
-PACKAGE config_const_pkg is
-
-    CONSTANT const_wordsize :integer := 8;
-
-    CONSTANT const_imageheight :integer := 480;
-    CONSTANT const_imagewidth  :integer := 640;
-    
-    CONSTANT const_hor_start_activeimage : integer := 10;
-    CONSTANT const_activeimagewidth: integer := const_imagewidth;
-
-    CONSTANT const_blanking_horizontal_left: integer := 10;
-END config_const_pkg;
-
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
-USE ieee.numeric_std.ALL;
-USE std.textio.ALL;
-
-USE work.image_io_pkg.ALL;
-USE work.config_const_pkg.ALL;
+use work.image_io_pkg.all;
+use work.config_const_pkg.all;
 
 --======================================================================================--
 
-ENTITY curve_adjust_tb IS
-  GENERIC (
-    wordsize:             INTEGER := const_wordsize; -- size of input pixel value in bits
+entity curve_adjust_tb is
+    generic (
+        wordsize:             integer := const_wordsize; --! size of input pixel value in bits
 
-    -- Image dimensions
-    imageheight:          INTEGER := const_imageheight;
-    imagewidth:           INTEGER := const_imagewidth
-  );
-END ENTITY;
+        -- image dimensions
+        --! TODO: Make image dimensions dependent on the input image
+        imageheight:          integer := const_imageheight; --! Image height used of the test_image
+        imagewidth:           integer := const_imagewidth   --! Image width used for the test_image
+    );
+end entity;
 
 --======================================================================================--
 
-ARCHITECTURE curve_adjust_tb OF curve_adjust_tb IS
+architecture curve_adjust_tb of curve_adjust_tb is
 
   --===================component declaration===================--
   --the design to test
-  COMPONENT curve_adjust IS
-    GENERIC (
+    component curve_adjust is
+    generic (
         wordsize:             integer  --! input image wordsize in bits
     );
-    PORT (
-        clk:                  in std_logic;       --! completely clocked process
-        rst:                  in std_logic;       --! asynchronous reset
-        enable:               in std_logic;       --! enables block
-        input_pixel:          in std_logic_vector((wordsize-1) downto 0);       --! the input pixel
+    port (
+        clk:           in std_logic;       --! completely clocked process
+        rst:           in std_logic;       --! asynchronous reset
+        enable:        in std_logic;       --! enables block
 
-        output_pixel:         out std_logic_vector((wordsize-1) downto 0)        --! the output pixel
+        pixel_i:       in std_logic_vector((wordsize-1) downto 0);       --! the input pixel
+
+        pixel_o:       out std_logic_vector((wordsize-1) downto 0)       --! the output pixel
     );
-  END COMPONENT;
+    end component;
 
-  ----------------------------------------------------------------------------------------------
+    ----------------------------------------------------------------------------------------------
 
-  --===================signal declaration===================--
-  SIGNAL tb_clk:                    STD_LOGIC := '0';
-  SIGNAL rst:                       STD_LOGIC := '0';
-  SIGNAL enable:                    STD_LOGIC := '0';
-  SIGNAL start:                     STD_LOGIC := '0';
+    --===================signal declaration===================--
+    signal tb_clk:                    std_logic := '0';
+    signal rst:                       std_logic := '0';
+    signal enable:                    std_logic := '0';
+    signal start:                     std_logic := '0';
+    signal end_of_file:               std_logic := '0';
 
-  SIGNAL tb_input_pixel:            STD_LOGIC_VECTOR((wordsize-1) DOWNTO 0) := STD_LOGIC_VECTOR(to_unsigned(0, const_wordsize));
-  SIGNAL tb_output_pixel:           STD_LOGIC_VECTOR((wordsize-1) DOWNTO 0) := STD_LOGIC_VECTOR(to_unsigned(0, const_wordsize));
+    signal tb_input_pixel:            std_logic_vector((wordsize-1) downto 0) := std_logic_vector(to_unsigned(0, const_wordsize));
+    signal tb_output_pixel:           std_logic_vector((wordsize-1) downto 0) := std_logic_vector(to_unsigned(0, const_wordsize));
 
-  SIGNAL hcount:                    STD_LOGIC_VECTOR(12 DOWNTO 0);
-  SIGNAL vcount:                    STD_LOGIC_VECTOR(10 DOWNTO 0);
 
-  --===================file declaration===================--
-  --opening a file in read mode for input pixels
-  FILE file_input_pixel: TEXT OPEN READ_MODE IS "../tst/input_pixel.txt";
+    --===================file declaration===================--
+    --! File containing pixels for input of the testbench
+    file file_input_pixel: text open read_mode is "tst/input_pixel.txt";
 
-  --opening a file in write mode for output
-  FILE file_output_pixel: TEXT OPEN WRITE_MODE IS "../tst/output_pixel.pgm";
+    --! File used as output for the tesbench
+    file file_output_pixel: text open write_mode is "tst/output_pixel.pgm";
 
-BEGIN
+begin
   --===================component instantiation===================--
 
-  -- instantiate a
+  --! Device Under Test
   dut: curve_adjust
-    GENERIC MAP(
+    generic map(
       wordsize          => wordsize -- size of input pixel value in bits
     )
 
-    PORT MAP(
+    port map(
       clk               => tb_clk,
       rst               => rst,
       enable            => enable,
 
-      input_pixel       => tb_input_pixel,
-
-      output_pixel      => tb_output_pixel
+      pixel_i           => tb_input_pixel,
+      pixel_o           => tb_output_pixel
     );
 
   --===================clock===================--
-  tb_clk <= NOT tb_clk AFTER 1 ns;
+  tb_clk <= not tb_clk after 1 ns;
 
   --===================rst===================--
-  rst <= '0', '1' AFTER 42 ns, '0' AFTER 85 ns;
+  rst <= '0', '1' after 42 ns, '0' after 85 ns;
 
   --=================== release ===============--
-  RELEASE_PROCESS: PROCESS(rst)
-  BEGIN
-    IF rst = '1' THEN
+  release_process: process(rst, end_of_file)
+  begin
+    if rst = '1' then
       start <= '1';
-      enable <= '1';  -- ENABLE tb
-    END IF;
-  END PROCESS;
+      enable <= '1';  -- enable tb
+    end if;
 
-  --===================line counters===================--
-  LINE_COUNTERS: PROCESS(tb_clk)
-    variable hcount_temp: unsigned(hcount'high downto 0) := "0000000000000";
-    variable vcount_temp: unsigned(vcount'high downto 0) := "00000010011";
-  BEGIN
-    IF RISING_EDGE(tb_clk) THEN
-      IF rst = '1' THEN
-        hcount      <= (others => '0');
-        vcount      <= STD_LOGIC_VECTOR(to_unsigned(19, vcount'length));--"00000010011";--(OTHERS => '0');
-
-      ELSE -- rst = '0'
-        IF hcount = STD_LOGIC_VECTOR(to_unsigned(const_hor_start_activeimage+const_activeimagewidth-1, hcount'length)) then --"1000111011111" THEN -- 4575
-          hcount_temp := to_unsigned(0, hcount_temp'length); --"0000000000000";
-          IF vcount = STD_LOGIC_VECTOR(to_unsigned(1124, vcount'length)) then --"10001100100" THEN  --1124
-            vcount_temp := to_unsigned(0, vcount_temp'length);--(OTHERS => '0');
-          ELSE
-            vcount_temp := unsigned(vcount) + 1;
-          END IF;
-        ELSIF hcount = STD_LOGIC_VECTOR(to_unsigned(const_blanking_horizontal_left-1, hcount'length)) THEN --"0001011001111" THEN  --719
-          hcount_temp := to_unsigned(0, hcount'length); --"0000000000000";
-          hcount_temp(12) := '1';
-        ELSE
-          hcount_temp := unsigned(hcount) + 1;
-        END IF;
-
-        hcount <= STD_LOGIC_VECTOR(hcount_temp);
-        vcount <= STD_LOGIC_VECTOR(vcount_temp);
-
-      END IF;
-    END IF;
-  END PROCESS;
+    if end_of_file = '1' then
+        enable <= '0';
+        start <= '0';
+    end if;
+  end process;
 
   --===================process for reading input_pixels ===============--
-  READING_INPUT_PIXELS: PROCESS(tb_clk)
-    VARIABLE li: LINE;
-    VARIABLE pixel_value: INTEGER;
-  BEGIN
-    IF RISING_EDGE(tb_clk) THEN
-      IF rst = '0' THEN
-        IF enable = '1' THEN
+  reading_input_pixels: process(tb_clk)
+    variable li: line;
+    variable pixel_value: integer;
+  begin
+    if rising_edge(tb_clk) then
+      if rst = '0' then
+        if enable = '1' then
 
-          IF (NOT ENDFILE(file_input_pixel)) THEN
-            READLINE(file_input_pixel, li);
-            READ(li, pixel_value);
-            tb_input_pixel<= STD_LOGIC_VECTOR(to_unsigned(pixel_value, tb_input_pixel'length));
-          END IF;
+          read_pixel(file_input_pixel, tb_input_pixel, end_of_file);
+          assert(end_of_file = '0');
 
-        END IF;
-      END IF;
-    END IF;
-  END PROCESS;
+        end if;
+      end if;
+    end if;
+  end process;
 
   --===================process for writing output ===================================--
-  WRITING_OUTPUT_FILE: PROCESS( tb_clk )
+  writing_output_file: process( tb_clk )
 
-    CONSTANT pgm_width         : INTEGER := const_imagewidth;
-    CONSTANT pgm_height        : INTEGER := const_imageheight;
-    CONSTANT max_pixel_value   : INTEGER := 2**wordsize-1;
+    constant pgm_width         : integer := const_imagewidth;
+    constant pgm_height        : integer := const_imageheight;
+    constant max_pixel_value   : integer := 2**wordsize-1;
 
-    VARIABLE writeheader: STD_LOGIC := '1';
+    variable writeheader: std_logic := '1';
 
-    VARIABLE val: INTEGER := 0;
+    variable val: integer := 0;
 
-  BEGIN
-    IF RISING_EDGE(tb_clk) THEN
+  begin
+    if rising_edge(tb_clk) then
 
-      IF writeheader = '1' THEN
+      if writeheader = '1' then
 
         write_pbmplus_header( pgm_width, pgm_height, max_pixel_value, pgm, file_output_pixel );
 
         writeheader := '0';
 
-      END IF;
+      end if;
 
-    -- write depth map...
-    val := to_INTEGER(unsigned(tb_output_pixel));
+    -- write output image 
+    val := to_integer(unsigned(tb_output_pixel));
     write_pixel( val, file_output_pixel);
 
-    END IF;
-  END PROCESS;
+    end if;
+  end process;
 
-END ARCHITECTURE;
+end architecture;
