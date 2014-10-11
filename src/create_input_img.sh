32efa90fc26bb5651faeb00f0897ebba969707d4
ben <ben.spoor@gmail.com>
2014-10-11 21:36:02 +0200

    Script now converts to value line-per-line

--- src/create_input_img.sh
+++ src/create_input_img.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 output_image=$1.pgm
- 
-convert $1 -compress none -depth 8 -set colorspace Gray -separate -average $output_image
+bits=8
+width=640 
+height=480
+
+# Creates an ascii based pgm gray image where all color channels are averaged into single gray channel 
+convert $1                                      \
+        -resize ${width}x${height}\!            \
+        -compress none -depth ${bits}           \
+        -set colorspace Gray -separate -average \
+        ${output_image}
+
+cat ${output_image} | sed 1,3d | sed 's/ \+/\n/g' | sed '/^$/d' > ${output_image}.txt
 
