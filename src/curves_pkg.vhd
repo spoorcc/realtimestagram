fa3cc4f12947930411c0b01725be8d6dd65668d0
ben <ben.spoor@gmail.com>
2014-10-12 14:51:50 +0200

    Improved curves_pkg documentation

--- src/curves_pkg.vhd
+++ src/curves_pkg.vhd
@@ -13,82 +13,89 @@
 --   You should have received a copy of the GNU General Public License
 --   along with Realtimestagram.  If not, see <http://www.gnu.org/licenses/>.
 
---! Curves package used to control the curve adjust component
---!
---! Curves
---! =========
---!
---! Multiple curves are possible:
---! * Straight (used for testing purposes) 
---! * Negate   (Negates all  pixel values) 
---! * Sigmoid  (used for contrast enhancement)
---! * Gamma    (used for contrast distribution)
---!
---! Straight
---! ---------- 
---! The straight function is \f[p_{out}=p_{in} \f]
---! \image html straight.png
---!
---! Negate
---! ---------- 
---! The negate function is \f[p_{out}=p_{max}-p_{in} \f]
---! \image html negate.png
---!
---! Sigmoid
---! ---------- 
---! The sigmoid function is \f[p_{out}=\frac{p_{max}}{1+\exp({-c/p_{max}*(p_{in}-p_{max})})} \f]
---! \image html sigmoid.png
---!
---! Gamma
---! ---------- 
---! The gamma function is \f[p_{out}=c*p_{max}*(\frac{p_{in}}{p_{max}})^{\gamma} \f]
---! \image html gamma.png
-
-
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use ieee.math_real.all;
 
+--! Package which provides functions to create Look Up Tables for various functions
+--! The generated Look Up Table can be placed in a lookup_table entity.
+--! \see lookup_table
 package curves_pkg is
 
-    constant wordsize   : integer := 8; 
-
-	type curvetype is ( straight, negate, sigmoid, gamma);
+    --! Bit depth of pixels
+    constant wordsize   : integer := 8;
 
     --! \brief array of std_logic_vectors
     type array_pixel is array (natural range <>) of std_logic_vector(wordsize-1 downto 0);
 
+    ----------------------------------------------------------------------     								
+    --! Function to create Look up table for a straight line
+    --! Used for testing purposes, creates a straight line.
+    --! The straight function is \f[p_{out}=p_{in} \f]
+    --! \param[in] size  Number of elements to create
+    --! \image html straight.png
+    function create_straight_lut( size:       integer)
 
-    function create_straight_lut(size:       integer)        --! Number of elements to create 
-                                 return array_pixel;
-
-    function create_negated_lut(size:       integer)        --! Number of elements to create 
-                                return array_pixel;
-
-    function create_sigmoid_lut(size:       integer;        --! Number of elements to create 
-                                c:          real := 1.0)    --! The amplification factor
-                             return array_pixel;
-
-    function create_gamma_lut( size:    integer; --! Number of elements to create 
-                               gamma:   real := 1.0;
-                               c:       real := 1.0)
                                   return array_pixel;
 
+    ----------------------------------------------------------------------     								
+    --! Function to create Look up table for a negated line
+    --! The negate function is \f[p_{out}=p_{max}-p_{in} \f]
+    --! \param[in] size  Number of elements to create
+    --! \image html negate.png
+    function create_negated_lut( size:       integer)
+
+                                 return array_pixel;
+
+    ----------------------------------------------------------------------     								
+    --! Function to create Look up table for a sigmoid function
+    --! The sigmoid function is \f[p_{out}=\frac{p_{max}}{1+\exp({-c/p_{max}*(p_{in}-p_{max})})} \f]
+    --! \param[in] size  Number of elements to create
+    --! \param[in] c     Amplification factor
+    --! \image html sigmoid.png
+    function create_sigmoid_lut( size:       integer;
+                                 c:          real := 1.0)
+
+                                 return array_pixel;
+
+    ----------------------------------------------------------------------     								
+    --! Function to create Look up table for a gamma function
+    --! The gamma function is \f[p_{out}=c*p_{max}*(\frac{p_{in}}{p_{max}})^{\gamma} \f]
+    --! \param[in] size  Number of elements to create
+    --! \param[in] gamma Gamma factor
+    --! \param[in] c     Amplification factor
+    --! \image html gamma.png
+    function create_gamma_lut( size:    integer;
+                               gamma:   real := 1.0;
+                               c:       real := 1.0)
+                               return array_pixel;
+
+    ----------------------------------------------------------------------     								
+    --! Procedure to assert that value can be represented in bit range
+    --! \param[in] value    Value to compare
+    --! \param[in] wordsize Bit depth
     procedure verify_valid_value( variable value:    in real;
                                   constant wordsize: in integer);
-                
+
+    ----------------------------------------------------------------------     								
+    --! Procedure to pretty print out a LUT value
+    --! \param[in] value    Value to be printed
+    --! \param[in] index    Index to print
     procedure report_lut_value( variable value:  in real;
                                 constant index:  in integer);
-    
+
 end curves_pkg;
 
 package body curves_pkg is
- 
-    function create_straight_lut( size:    integer) --! Number of elements to create 
-                                  return array_pixel is       
-        variable calc_val: real := 0.0;                 --! The calculated real_value 
-        variable return_value: array_pixel(0 to size-1);   --! 
+
+--======================================================================================--
+    function create_straight_lut( size:    integer)
+                                  return array_pixel is
+
+        variable calc_val:     real := 0.0;
+        variable return_value: array_pixel(0 to size-1);
+
     begin
 
         for i in return_value'range loop
@@ -100,13 +107,17 @@ package body curves_pkg is
         end loop;
 
         return return_value;
-    end  create_straight_lut;
+    end create_straight_lut;
 
-    function create_negated_lut( size:    integer) --! Number of elements to create 
-                                  return array_pixel is       
-        variable calc_val: real := 0.0;                 --! The calculated real_value 
-        constant max_val:  real := real(2**wordsize)-1.0; --! The maximum value possible, used in calculation and asserting the values are in range
-        variable return_value: array_pixel(0 to size-1);   --! 
+--======================================================================================--
+
+    function create_negated_lut( size:    integer)
+                                 return array_pixel is
+
+        variable calc_val:      real := 0.0;
+        constant max_val:       real := real(2**wordsize)-1.0;
+        variable return_value:  array_pixel(0 to size-1);
+
     begin
 
         for i in return_value'range loop
@@ -120,13 +131,16 @@ package body curves_pkg is
         return return_value;
     end  create_negated_lut;
 
-    function create_sigmoid_lut(size:       integer;                     --! Number of elements to create 
-                                c:          real := 1.0)    --! The amplification factor
-                                  return array_pixel is       
-        variable exponent: real := 0.0;                 --! temp variable used for calculation
-        variable calc_val: real := 0.0;                 --! The calculated real_value 
-        constant max_val:  real := real(2**wordsize)-1.0; --! The maximum value possible, used in calculation and asserting the values are in range
-        variable return_value: array_pixel(0 to size-1);   --! 
+--======================================================================================--
+
+    function create_sigmoid_lut( size:    integer;
+                                 c:       real := 1.0)
+                                 return   array_pixel is
+
+        variable exponent:      real := 0.0;
+        variable calc_val:      real := 0.0;
+        constant max_val:       real := real(2**wordsize)-1.0;
+        variable return_value:  array_pixel(0 to size-1);
     begin
 
         for i in return_value'range loop
@@ -144,13 +158,17 @@ package body curves_pkg is
         return return_value;
     end  create_sigmoid_lut;
 
-    function create_gamma_lut( size:    integer; --! Number of elements to create 
+--======================================================================================--
+
+    function create_gamma_lut( size:    integer;
                                gamma:   real := 1.0;
                                c:       real := 1.0)
-                                  return array_pixel is       
-        variable calc_val: real := 0.0;   --! The calculated real_value 
-        constant max_val:  real := real(2**wordsize)-1.0; --! The maximum value possible, used in calculation and asserting the values are in range
-        variable return_value: array_pixel(0 to size-1);   --! 
+                               return   array_pixel is
+
+        variable calc_val: real := 0.0;
+        constant max_val:  real := real(2**wordsize)-1.0;
+        variable return_value: array_pixel(0 to size-1);
+
     begin
 
         for i in return_value'range loop
@@ -167,14 +185,21 @@ package body curves_pkg is
         return return_value;
     end  create_gamma_lut;
 
+--======================================================================================--
 
     procedure verify_valid_value( variable value:    in real;
                                   constant wordsize: in integer) is
+
         constant max_val : integer := integer(2**wordsize)-1;
+
     begin
+
         assert(integer(value) <= max_val) report "LUT filled with invalid value: " & integer'image(integer(value)) severity failure;
         assert(integer(value) >= 0) report "LUT filled with invalid value" severity failure;
+
     end procedure verify_valid_value;
+
+--======================================================================================--
 
     procedure report_lut_value( variable value:  in real;
                                 constant index:  in integer) is
