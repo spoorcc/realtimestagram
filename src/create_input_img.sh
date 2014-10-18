c6ed569a4e1143571fbd2277cba919704086e70b
ben <ben.spoor@gmail.com>
2014-10-19 01:34:42 +0200

    Adapted creation script for color images

--- src/create_input_img.sh
+++ src/create_input_img.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 
-output_image=$1.pgm
 bits=8
 width=512 
 height=512
 
 # Creates an ascii based pgm gray image where all color channels are averaged into single gray channel 
-convert $1                                      \
-        -resize ${width}x${height}\!            \
-        -compress none -depth ${bits}           \
-        -set colorspace Gray -separate -average \
-        ${output_image}
+function create_gray_image {
+    output_image=$1.pgm
 
-cat ${output_image} | sed 1,3d | sed 's/ \+/\n/g' | sed '/^$/d' > ${output_image}.txt
+    convert $1                                      \
+            -resize ${width}x${height}\!            \
+            -compress none -depth ${bits}           \
+            -set colorspace Gray -separate -average \
+            ${output_image}
+}
 
+function create_color_image {
+    output_image=$1.pnm
+
+    convert $1                                      \
+            -resize ${width}x${height}\!            \
+            -compress none -depth ${bits}           \
+            ${output_image}
+}
+
+function remove_pgm_header_and_split {
+    cat ${output_image} | sed 1,3d | sed 's/ \+/\n/g' | sed '/^$/d' > ${output_image}.txt
+}
+
+function remove_pnm_header_and_split {
+    cat ${output_image} | sed 1,3d | sed 's/\([0-9]\+ [0-9]\+ [0-9]\+\) /\1\n/g' | sed '/^$/d' > ${output_image}.txt
+}
+
+create_color_image $1
+remove_pnm_header_and_split
