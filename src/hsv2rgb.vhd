175836caf60e358d9b259fd09c321eaf5e97aeaa
ben <ben.spoor@gmail.com>
2014-11-14 23:58:14 +0100

    Some progression in hsv2rgb implementation

--- src/hsv2rgb.vhd
+++ src/hsv2rgb.vhd
@@ -57,28 +57,33 @@ entity hsv2rgb is
     pixel_blue_o:         out std_logic_vector((wordsize-1) downto 0)  --! blue output pixel
   );
 
-    type s_t_v_delay       is array(0 to 2) of integer range 0 to 2**(wordsize+1);
-    type s_t_v_min_v_delay is array(0 to 3) of integer range 0 to 2**(wordsize+1);
+    -- Types for radhakrishan architecture
+    type s_t_v_delay       is array(0 to 2) of integer range 0 to 2**(wordsize*2);
+    type s_t_v_min_v_delay is array(0 to 3) of integer range 0 to 2**(wordsize*2);
     type v_delay           is array(0 to 5) of integer range 0 to 2**wordsize;
+
+    -- Types for bailey architecture
+    type val_delay         is array(0 to 2) of integer range 0 to 2**wordsize;
+    type h_msb_delay       is array(0 to 2) of integer range 0 to 6;
 
 end entity;
 
 --============================================================================--
 
-architecture behavioural of hsv2rgb is
+architecture presentation of hsv2rgb is
     
     -- signal declarations
-    signal hue_times_3:            integer range 0 to 3 * 2**wordsize;   --! Input Hue pixel * 3
-    signal hue_t3_div128:          integer range 0 to 128;               --! 128 / hue_times_3
-    signal hue_t3_d128_t8192:      integer range 0 to 8192*128;          --! 8192 * (128 / hue_times_3)
+    signal hue_times_3:            integer range 0 to 3 * 2**wordsize;       --! Input Hue pixel * 3
+    signal hue_t3_div128:          integer range 0 to 3 * 2**wordsize;       --! 128 / hue_times_3
+    signal hue_t3_d128_t8192:      integer range 0 to 8192*3 * 2**wordsize;  --! 8192 * (128 / hue_times_3)
     
     signal hue_d0:                 integer range 0 to 2**wordsize;       --! Delayed input Hue
     signal hue_t255:               integer range 0 to 255*(2**wordsize); --! input Hue * 255
     signal hue_t255_d0:            integer range 0 to 255*(2**wordsize); --! Delayed input Hue * 255
 
-    signal hue_min:                integer range 0 to 8192*128; --! 8192 * (128 / hue_times_3) - 255 * Hue
-    signal hue_min_d0:             integer range 0 to 8192*128; --! 8192 * (128 / hue_times_3) - 255 * Hue
-    signal hue_min_d1:             integer range 0 to 8192*128; --! 8192 * (128 / hue_times_3) - 255 * Hue
+    signal hue_min:                integer range 0 to 8192*3 * 2**wordsize; --! 8192 * (128 / hue_times_3) - 255 * Hue
+    signal hue_min_d0:             integer range 0 to 8192*3 * 2**wordsize; --! 8192 * (128 / hue_times_3) - 255 * Hue
+    signal hue_min_d1:             integer range 0 to 8192*3 * 2**wordsize; --! 8192 * (128 / hue_times_3) - 255 * Hue
 
     signal hue_times_val_sat:      integer range 0 to 8192*128*2**(wordsize+1); --! Hue * Val*Sat
 
@@ -137,20 +142,29 @@ begin
                 val_i_int := to_integer(unsigned(pixel_val_i));
 
                 hue_times_3 <= hue_i_int * 3;
-                hue_t3_div128 <= 128 / hue_times_3;         
+                hue_t3_div128 <= hue_times_3 / 128;
                 hue_t3_d128_t8192 <= hue_t3_div128 * 8192;     
                                        
                 hue_d0 <= hue_i_int;     
                 hue_t255 <= hue_d0 * 255;   
                 hue_t255_d0 <= hue_t255;
                                        
-                hue_min <= hue_t3_d128_t8192 - hue_t255_d0;
+                if hue_t3_d128_t8192 >= hue_t255_d0 then
+                    hue_min    <= hue_t3_d128_t8192 - hue_t255_d0;
+                else
+                    hue_min    <= 0;
+                end if;
+
                 hue_min_d0 <= hue_min;
                 hue_min_d1 <= hue_min_d0;
 
                 hue_times_val_sat <= hue_min * sat_times_val(2);
 
-                hue_t_val_sat_min_val <= hue_times_val_sat - val(4);
+                if hue_times_val_sat /= 0 then
+                    hue_t_val_sat_min_val <= hue_times_val_sat - val(4);
+                else
+                    hue_t_val_sat_min_val <= 0;
+                end if;
 
                 hue_t_val_sat_plus_sat <= hue_times_val_sat + sat_t_val_minus_val(2);
 
@@ -203,6 +217,124 @@ begin
                 pixel_red_o <= (others => '0');
                 pixel_green_o <= (others => '0');
                 pixel_blue_o <= (others => '0');
+            end if; -- end if enable = '1'
+
+        end if; -- end if rst = '1'
+    end process;
+
+end architecture;
+--============================================================================--
+
+architecture bailey of hsv2rgb is
+
+    constant degrees60:             integer := 60*256/360;
+    
+    signal val_times_sat:           integer range 0 to 2**(wordsize*2);
+
+    signal val_min_valsat:          integer range 0 to 2**(wordsize*2);
+
+    --signal hue_lsb:                 integer range 0 to degrees60;
+    signal hue_lsb:                 integer range 0 to 2**wordsize;
+    signal hue_msb:                 h_msb_delay;
+
+    signal hue_msb_odd:             integer range 0 to 1;
+
+    signal one_min_hue_lsb:         integer range 0 to 2**wordsize;
+
+    signal valsat_t_hue_lsb:        integer range 0 to 2**(wordsize*3);
+ 
+    signal val:                     val_delay;
+begin
+
+    hsv2rgb : process(clk, rst)
+
+        variable hue_i_int : integer range 0 to 2**wordsize := 0;
+        variable sat_i_int : integer range 0 to 2**wordsize := 0;
+        variable val_i_int : integer range 0 to 2**wordsize := 0;
+        
+    begin
+        if rst = '1' then
+
+           val_times_sat <= 0;
+
+           val_min_valsat <= 0;
+
+           hue_lsb <= 0;
+           hue_msb <= (others => 0);
+
+           hue_msb_odd <= 0;
+
+           one_min_hue_lsb <= 0;
+
+           valsat_t_hue_lsb <= 0; 
+
+           val <= (others => 0);
+
+        elsif rising_edge(clk) then
+
+            if enable = '1' then
+                
+                hue_i_int := to_integer(unsigned(pixel_hue_i));
+                sat_i_int := to_integer(unsigned(pixel_sat_i));
+                val_i_int := to_integer(unsigned(pixel_val_i));
+
+                val_times_sat <= val_i_int * sat_i_int;
+
+                val(0) <= val_i_int;
+                val(1 to 2) <= val(0 to 1);
+
+                val_min_valsat <= 2**wordsize * val(0) - val_times_sat;
+
+                hue_lsb    <= (hue_i_int mod degrees60) * 6; 
+                hue_msb(0) <= hue_i_int / degrees60; 
+
+                hue_msb_odd <= hue_msb(0) mod 2;
+
+                hue_msb(1 to 2) <= hue_msb(0 to 1);
+
+                if hue_msb_odd = 1 then
+                    one_min_hue_lsb <= 2**wordsize - hue_lsb;
+                else
+                    one_min_hue_lsb <= hue_lsb;
+                end if;
+
+                valsat_t_hue_lsb <= one_min_hue_lsb * val_times_sat; 
+
+                -- Mux output selection
+                output_mux: case hue_msb(2) is
+                    when 5 =>
+                        pixel_red_o   <= std_logic_vector(to_unsigned(val(2), wordsize));
+                        pixel_green_o <= std_logic_vector(to_unsigned(val_min_valsat/2**wordsize, wordsize));
+                        pixel_blue_o  <= std_logic_vector(to_unsigned(valsat_t_hue_lsb/2**(wordsize*2), wordsize));
+                    when 0 =>
+                        pixel_red_o   <= std_logic_vector(to_unsigned(val(2), wordsize));
+                        pixel_green_o <= std_logic_vector(to_unsigned(valsat_t_hue_lsb/2**(wordsize*2), wordsize));
+                        pixel_blue_o  <= std_logic_vector(to_unsigned(val_min_valsat/2**wordsize, wordsize));
+                    when 1 =>
+                        pixel_red_o   <= std_logic_vector(to_unsigned(valsat_t_hue_lsb/2**(wordsize*2), wordsize));
+                        pixel_green_o <= std_logic_vector(to_unsigned(val(2), wordsize));
+                        pixel_blue_o  <= std_logic_vector(to_unsigned(val_min_valsat/2**wordsize, wordsize));
+                    when 2 =>
+                        pixel_red_o   <= std_logic_vector(to_unsigned(val_min_valsat, wordsize));
+                        pixel_green_o <= std_logic_vector(to_unsigned(val(2), wordsize));
+                        pixel_blue_o  <= std_logic_vector(to_unsigned(valsat_t_hue_lsb/2**(wordsize*2), wordsize));
+                    when 3 =>
+                        pixel_red_o   <= std_logic_vector(to_unsigned(val_min_valsat/2**wordsize, wordsize));
+                        pixel_green_o <= std_logic_vector(to_unsigned(valsat_t_hue_lsb/2**(wordsize*2), wordsize));
+                        pixel_blue_o  <= std_logic_vector(to_unsigned(val(2), wordsize));
+                    when 4 =>
+                        pixel_red_o   <= std_logic_vector(to_unsigned(valsat_t_hue_lsb/2**(wordsize*2), wordsize));
+                        pixel_green_o <= std_logic_vector(to_unsigned(val_min_valsat/2**wordsize, wordsize));
+                        pixel_blue_o  <= std_logic_vector(to_unsigned(val(2), wordsize));
+                    when others =>
+                        pixel_red_o   <= (others => '0');
+                        pixel_green_o <= (others => '0');
+                        pixel_blue_o  <= (others => '0');
+                end case output_mux;
+            else
+                pixel_red_o   <= (others => '0');
+                pixel_green_o <= (others => '0');
+                pixel_blue_o  <= (others => '0');
             end if; -- end if enable = '1'
 
         end if; -- end if rst = '1'
---
+++ src/hsv2rgb_tb.vhd
@@ -0,0 +1,156 @@
+--   This file is part of Realtimestagram.
+--
+--   Realtimestagram is free software: you can redistribute it and/or modify
+--   it under the terms of the GNU General Public License as published by
+--   the Free Software Foundation, either version 2 of the License, or
+--   (at your option) any later version.
+--
+--   Realtimestagram is distributed in the hope that it will be useful,
+--   but WITHOUT ANY WARRANTY; without even the implied warranty of
+--   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--   GNU General Public License for more details.
+--
+--   You should have received a copy of the GNU General Public License
+--   along with Realtimestagram.  If not, see <http://www.gnu.org/licenses/>.
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+use work.config_const_pkg.all;
+
+--! Used for calculation of h_count and v_count port width
+use ieee.math_real.all;
+
+--======================================================================================--
+
+entity hsv2rgb_tb is
+    generic (
+        input_file:           string  := "tst/output/rgb2hsv_rainbow.pnm"; --! Input file of test 
+        output_file:          string  := "tst/output/hsv2rgb_output.pnm"; --! Output file of test 
+
+        image_width:          integer := const_imagewidth; --! Width of input image
+        image_height:         integer := const_imageheight  --! Height of input image
+    );
+end entity;
+
+--======================================================================================--
+
+architecture structural of hsv2rgb_tb is
+
+  --===================component declaration===================--
+
+    component test_bench_driver_color is
+        generic (
+            wordsize:           integer := const_wordsize;
+
+            input_file:         string := input_file;
+            output_file:        string := output_file;
+
+            clk_period_ns:      time := 1 ns;
+            rst_after:          time := 9 ns;
+            rst_duration:       time := 8 ns;
+
+            dut_delay:          integer := 4
+        );
+        port (
+            clk:                out std_logic;
+            rst:                out std_logic;
+            enable:             out std_logic;
+
+            h_count:            out std_logic_vector;
+            v_count:            out std_logic_vector;
+
+            red_pixel_from_file:    out std_logic_vector;
+            green_pixel_from_file:  out std_logic_vector;
+            blue_pixel_from_file:   out std_logic_vector;
+
+            red_pixel_to_file:      in std_logic_vector;
+            green_pixel_to_file:    in std_logic_vector;
+            blue_pixel_to_file:     in std_logic_vector
+        );
+    end component;
+
+    ----------------------------------------------------------------------------------------------
+
+    component hsv2rgb is
+        generic (
+            wordsize:             integer := 8    --! input image wordsize in bits
+        );
+        port (
+
+            -- inputs
+            clk:                  in std_logic;       --! completely clocked process
+            rst:                  in std_logic;       --! asynchronous reset
+            enable:               in std_logic;       --! enables block
+        
+            pixel_hue_i:          in std_logic_vector;
+            pixel_sat_i:          in std_logic_vector;
+            pixel_val_i:          in std_logic_vector;
+
+            -- outputs
+            pixel_red_o:          out std_logic_vector;
+            pixel_green_o:        out std_logic_vector;
+            pixel_blue_o:         out std_logic_vector
+
+        );
+    end component;
+
+    for device_under_test : hsv2rgb use entity work.hsv2rgb(bailey);
+
+    ----------------------------------------------------------------------------------------------
+
+    --===================signal declaration===================--
+    signal clk:                      std_logic := '0';
+    signal rst:                      std_logic := '0';
+    signal enable:                   std_logic := '0';
+
+    signal h_count:                  std_logic_vector((integer(ceil(log2(real(image_width))))-1) downto 0) := (others => '0');
+    signal v_count:                  std_logic_vector((integer(ceil(log2(real(image_height))))-1) downto 0) := (others => '0');
+
+    signal red_pixel_from_file:      std_logic_vector((const_wordsize-1) downto 0) := (others => '0');
+    signal green_pixel_from_file:    std_logic_vector((const_wordsize-1) downto 0) := (others => '0');
+    signal blue_pixel_from_file:     std_logic_vector((const_wordsize-1) downto 0) := (others => '0');
+
+    signal red_pixel_to_file:        std_logic_vector((const_wordsize-1) downto 0) := (others => '0');
+    signal green_pixel_to_file:      std_logic_vector((const_wordsize-1) downto 0) := (others => '0');
+    signal blue_pixel_to_file:       std_logic_vector((const_wordsize-1) downto 0) := (others => '0');
+
+begin
+
+    --===================component instantiation===================--
+    tst_driver: test_bench_driver_color
+        port map(
+            clk             => clk,
+            rst             => rst,
+            enable          => enable,
+        
+            h_count         => h_count,
+            v_count         => v_count,
+
+            red_pixel_from_file   => red_pixel_from_file,
+            green_pixel_from_file => green_pixel_from_file,
+            blue_pixel_from_file  => blue_pixel_from_file,
+
+            red_pixel_to_file   => red_pixel_to_file,
+            green_pixel_to_file => green_pixel_to_file,
+            blue_pixel_to_file  => blue_pixel_to_file
+        );
+
+    device_under_test: hsv2rgb
+        port map(
+            clk             => clk,
+            rst             => rst,
+            enable          => enable,
+
+            pixel_hue_i     => red_pixel_from_file,
+            pixel_sat_i     => green_pixel_from_file,
+            pixel_val_i     => blue_pixel_from_file,
+
+            pixel_red_o     => red_pixel_to_file,
+            pixel_green_o   => green_pixel_to_file,
+            pixel_blue_o    => blue_pixel_to_file
+
+        );
+
+end architecture;
