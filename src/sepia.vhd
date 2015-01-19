001f660e6808443bf349779ffd41d2505e82bd19
ben <ben.spoor@gmail.com>
2015-01-19 22:14:50 +0100

    Implemented sepia according to image_magick filter

--- src/sepia.vhd
+++ src/sepia.vhd
@@ -24,10 +24,6 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
---! Used for calculation of h_count and v_cunt port width
-use ieee.math_real.all;
-
-use work.curves_pkg.all;
 --============================================================================--
 --!
 --!
@@ -37,9 +33,7 @@ entity sepia is
   generic (
     wordsize:             integer;    --! input image wordsize in bits
     image_width:          integer;    --! width of input image
-    image_height:         integer;    --! height of input image
-
-    amount:               real        --! amount of sepia effect [0.0 - 1.0]
+    image_height:         integer     --! height of input image
 
   );
   port (
@@ -51,87 +45,58 @@ entity sepia is
     pixel_green_i:        in std_logic_vector((wordsize-1) downto 0); --! the input pixel
     pixel_blue_i:         in std_logic_vector((wordsize-1) downto 0); --! the input pixel
 
+    threshold:            in std_logic_vector((wordsize-1) downto 0); --! Amount of sepia effect
+
     pixel_red_o:          out std_logic_vector((wordsize-1) downto 0); --! the input pixel
     pixel_green_o:        out std_logic_vector((wordsize-1) downto 0); --! the input pixel
     pixel_blue_o:         out std_logic_vector((wordsize-1) downto 0)  --! the input pixel
   );
 
-
---  constant RR  : real := 0.299 ;
---  constant RG  : real := 0.587 ;
---  constant RB  : real := 0.114 ;
-
---  constant GR  : real := 0.299 ;
---  constant GG  : real := 0.587 ;
---  constant GB  : real := 0.114 ;
-
---  constant BR  : real := 0.299 ;
---  constant BG  : real := 0.587 ;
---  constant BB  : real := 0.114 ;
-
-    constant RR  : real := 0.393 ;
-    constant RG  : real := 0.769 ;
-    constant RB  : real := 0.189 ;
-  
-    constant GR  : real := 0.349 ;
-    constant GG  : real := 0.686 ;
-    constant GB  : real := 0.168 ;
-  
-    constant BR  : real := 0.272 ;
-    constant BG  : real := 0.534 ;
-    constant BB  : real := 0.131 ;
-
---  constant RR  : real := 0.291; --0.393 ;
---  constant RG  : real := 0.569; --0.769 ;
---  constant RB  : real := 0.140; --0.189 ;
-
---  constant GR  : real := 0.258; --0.349 ;
---  constant GG  : real := 0.508; --0.686 ;
---  constant GB  : real := 0.124; --0.168 ;
-
---  constant BR  : real := 0.201; --0.272 ;
---  constant BG  : real := 0.395; --0.534 ;
---  constant BB  : real := 0.097; --0.131 ;
 end entity;
 
 --============================================================================--
 
 architecture behavioural of sepia is
-    
-    constant lut_rr:           array_pixel := create_straight_lut(2**wordsize, RR * amount);
-    constant lut_rg:           array_pixel := create_straight_lut(2**wordsize, RG * amount);
-    constant lut_rb:           array_pixel := create_straight_lut(2**wordsize, RB * amount);
 
-    constant lut_gr:           array_pixel := create_straight_lut(2**wordsize, GR * amount);
-    constant lut_gg:           array_pixel := create_straight_lut(2**wordsize, GG * amount);
-    constant lut_gb:           array_pixel := create_straight_lut(2**wordsize, GB * amount);
+    constant max_value  : integer := 2**wordsize - 1;
+ 
+    signal red_0        : integer range 0 to max_value;
+    signal green_0      : integer range 0 to max_value;
+    signal blue_0       : integer range 0 to max_value;
+     
+    signal r_g_max      : integer range 0 to max_value;
+    signal b_delay      : integer range 0 to max_value;
+    signal intensity    : integer range 0 to max_value;
 
-    constant lut_br:           array_pixel := create_straight_lut(2**wordsize, BR * amount);
-    constant lut_bg:           array_pixel := create_straight_lut(2**wordsize, BG * amount);
-    constant lut_bb:           array_pixel := create_straight_lut(2**wordsize, BB * amount);
+    signal th_delay_d0  : integer range 0 to max_value;
+    signal th_delay_d1  : integer range 0 to max_value;
 
-    signal red_0   : integer range 0 to 2**(wordsize + 1);
-    signal green_0 : integer range 0 to 2**(wordsize + 1);
-    signal blue_0  : integer range 0 to 2**(wordsize + 1);
+    signal th_diff_6    : integer range 0 to max_value/6;
+    signal th_d6_delay  : integer range 0 to max_value/6;
+    signal th_d6_times7 : integer range 0 to (max_value/6)*7;
+
+    signal max_min_th   : integer range 0 to max_value;
+
+    signal tone         : integer range 0 to max_value / 7;
+
+    signal red_o_int   : integer range 0 to max_value;
+    signal green_o_int : integer range 0 to max_value;
+    signal blue_o_int  : integer range 0 to max_value;
 
 begin
 
-    --! \brief clocked process that outputs LUT-value on each rising edge if enable is true 
-    --! \param[in] clk clock
-    --! \param[in] rst asynchronous reset
-    apply_rgb_luts : process(clk, rst)
+    sepia_process : process(clk, rst)
 
-        variable red_i_int   : integer range 0 to 2**wordsize := 0;
-        variable green_i_int : integer range 0 to 2**wordsize := 0;
-        variable blue_i_int  : integer range 0 to 2**wordsize := 0;
+        variable red_i_int   : integer range 0 to max_value := 0;
+        variable green_i_int : integer range 0 to max_value := 0;
+        variable blue_i_int  : integer range 0 to max_value := 0;
 
-        variable red_o_int   : integer range 0 to 2**(wordsize + 2) := 0;
-        variable green_o_int : integer range 0 to 2**(wordsize + 2) := 0;
-        variable blue_o_int  : integer range 0 to 2**(wordsize + 2) := 0;
+        variable threshold_i_int  : integer range 0 to max_value := 0;
 
-        variable pixel_red_slv   : std_logic_vector(wordsize+1 downto 0) := (others => '0');
-        variable pixel_green_slv : std_logic_vector(wordsize+1 downto 0) := (others => '0');
-        variable pixel_blue_slv  : std_logic_vector(wordsize+1 downto 0) := (others => '0');
+        variable red_o_int_d0   : integer range 0 to max_value := 0;
+        variable green_o_int_d0 : integer range 0 to max_value := 0;
+        variable blue_o_int_d0  : integer range 0 to max_value := 0;
+
     begin
 
         if rst = '1' then
@@ -140,9 +105,28 @@ begin
             pixel_green_o <= (others => '0');
             pixel_blue_o  <= (others => '0');
 
-            red_0   <= 0;
-            green_0 <= 0;
-            blue_0  <= 0;
+            r_g_max      <= 0;
+            b_delay      <= 0;
+            intensity    <= 0;
+
+            th_delay_d0  <= 0;
+            th_delay_d1  <= 0;
+
+            th_diff_6    <= 0;
+            th_d6_delay  <= 0;
+            th_d6_times7 <= 0;
+
+            max_min_th   <= 0;
+
+            tone         <= 0;
+
+            red_o_int    <= 0;
+            green_o_int  <= 0;
+            blue_o_int   <= 0;
+
+            red_o_int_d0   := 0;    
+            green_o_int_d0 := 0;
+            blue_o_int_d0  := 0;
 
         elsif rising_edge(clk) then
 
@@ -152,39 +136,74 @@ begin
                 green_i_int := to_integer(unsigned(pixel_green_i));
                 blue_i_int  := to_integer(unsigned(pixel_blue_i));
 
-                -- first stage
-                red_0   <= to_integer(unsigned(lut_rr(red_i_int)))   + to_integer(unsigned(lut_rg(red_i_int)));
-                green_0 <= to_integer(unsigned(lut_gr(green_i_int))) + to_integer(unsigned(lut_gg(green_i_int)));
-                blue_0  <= to_integer(unsigned(lut_br(blue_i_int)))  + to_integer(unsigned(lut_bg(blue_i_int)));
-
-                -- Second stage
-                red_o_int   := red_0   + to_integer(unsigned(lut_rb(red_i_int)));
-                green_o_int := green_0 + to_integer(unsigned(lut_gb(green_i_int)));
-                blue_o_int  := blue_0  + to_integer(unsigned(lut_bb(blue_i_int)));
-
-                -- Clip and output red channel
-                if red_o_int >= 2**wordsize then
-                    pixel_red_slv := (others => '1');
+                -- Convert to gray, by finding largest value
+                if red_i_int >= green_i_int then
+                    r_g_max <= red_i_int;
                 else
-                    pixel_red_slv := std_logic_vector(to_unsigned(red_o_int, wordsize+2));
-                end if; 
-                pixel_red_o   <= pixel_red_slv(wordsize-1 downto 0);
-
-                -- Clip and output green channel
-                if green_o_int >= 2**wordsize then
-                    pixel_green_slv := (others => '1');
-                else
-                    pixel_green_slv := std_logic_vector(to_unsigned(green_o_int, wordsize+2));
+                    r_g_max <= green_i_int;
                 end if;
-                pixel_green_o   <= pixel_green_slv(wordsize-1 downto 0);
+                b_delay <= blue_i_int;
 
-                -- Clip and output blue channel
-                if blue_o_int >= 2**wordsize then
-                    pixel_blue_slv := (others => '1');
+                if r_g_max >= b_delay then
+                    intensity <= r_g_max;
                 else
-                    pixel_blue_slv := std_logic_vector(to_unsigned(blue_o_int, wordsize+2));
+                    intensity <= b_delay;
                 end if;
-                pixel_blue_o   <= pixel_blue_slv(wordsize-1 downto 0);
+
+                -- Calculate threshold
+                threshold_i_int := to_integer(unsigned(threshold));
+
+                th_diff_6 <= threshold_i_int / 6;
+                th_d6_delay <= th_diff_6;
+
+                th_d6_times7 <= th_diff_6 * 7;
+
+                th_delay_d0 <= threshold_i_int;                
+                th_delay_d1 <= th_delay_d0;
+
+                max_min_th <= max_value - th_delay_d0;
+
+                -- Calculate red pixel
+                if intensity > th_delay_d1 then
+                    red_o_int <= max_value;
+                else
+                    red_o_int <= intensity + max_min_th;
+                end if;
+
+                -- Calculate green pixel
+                if intensity > th_d6_times7 then
+                    green_o_int <= max_value;
+                else
+                    green_o_int <= intensity + max_value - th_d6_times7;
+                end if;
+
+                -- Calculate blue pixel
+                if intensity < th_d6_delay then
+                    blue_o_int <= 0;
+                else
+                    blue_o_int <= intensity - th_d6_delay;
+                end if;
+
+                -- Check ranges
+                tone <= th_delay_d1 / 7;
+
+                red_o_int_d0 := red_o_int;
+
+                if green_o_int < tone then
+                    green_o_int_d0 := tone;
+                else
+                    green_o_int_d0 := green_o_int;
+                end if;
+
+                if blue_o_int < tone then
+                    blue_o_int_d0 := tone;
+                else
+                    blue_o_int_d0 := blue_o_int;
+                end if;
+ 
+                pixel_red_o   <= std_logic_vector(to_unsigned(red_o_int_d0, wordsize));
+                pixel_green_o <= std_logic_vector(to_unsigned(green_o_int_d0, wordsize));
+                pixel_blue_o  <= std_logic_vector(to_unsigned(blue_o_int_d0, wordsize));
 
             else
               pixel_red_o   <= (others => '0');
