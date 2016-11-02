94018460653f6f1f9b134d5a88db5547974bd7bd
ben <ben.spoor@gmail.com>
2016-11-02 22:31:21 +0100

    mend

--- src/rgb2hsv/src/rgb2hsv.vhd
+++ src/rgb2hsv/src/rgb2hsv.vhd
@@ -35,7 +35,10 @@ use ieee.math_real.all;
 --! # Features
 --!
 --! - 6 clk delay
+--! - Asynchronous reset
+--! - Configurable input wordsize
 --! - fully parameterized testbench
+--! - 3 multipliers (for hue calculation)
 --!
 --! # Functional description
 --!
@@ -44,15 +47,15 @@ use ieee.math_real.all;
 --! # Algorithm {#rgb2hsv_algorithm}
 --!
 --! ## Hue
---! The Hue indicates the degrees on the color circle. Starting at 0 degrees with red, 
---! 120 degrees for green and 240 degrees for blue. 
---! Because 360 degrees does map not correctly on the 8 bits of a byte everything is normalised 
+--! The Hue indicates the degrees on the color circle. Starting at 0 degrees with red,
+--! 120 degrees for green and 240 degrees for blue.
+--! Because 360 degrees does map not correctly on the 8 bits of a byte everything is normalised
 --! to the full range of a byte
 --! Hue is calculated following the function:
 --!
 --! \f[H =\left\{\begin{matrix} 0, & R=G=B\\
 --! \frac{(G-B)*60^{\circ}}{max(R,G,B)-min(R,G,B)}\textup{mod}\:360^{\circ}, & R \geq G,B\\
---! \frac{(B-R)*60^{\circ}}{max(R,G,B)-min(R,G,B)}+120^{\circ}, & G \geq R,B\\ 
+--! \frac{(B-R)*60^{\circ}}{max(R,G,B)-min(R,G,B)}+120^{\circ}, & G \geq R,B\\
 --! \frac{(R-G)*60^{\circ}}{max(R,G,B)-min(R,G,B)}+240^{\circ} & B \geq R,G  \\
 --! \end{matrix}\right.\f]
 --!
@@ -72,6 +75,11 @@ use ieee.math_real.all;
 --!
 --! > TODO calculate the global quantization noise power
 --!
+--! # Known issues
+--!
+--! - Currently limited to use for 8-bit words
+--! - Potentially unneeded multiplier in saturation calculation
+--!
 
 entity rgb2hsv is
   generic (
@@ -83,7 +91,7 @@ entity rgb2hsv is
     clk:                  in std_logic;       --! completely clocked process
     rst:                  in std_logic;       --! asynchronous reset
     enable:               in std_logic;       --! enables block
- 
+
     pixel_red_i:          in std_logic_vector((wordsize-1) downto 0); --! red input pixel
     pixel_green_i:        in std_logic_vector((wordsize-1) downto 0); --! green input pixel
     pixel_blue_i:         in std_logic_vector((wordsize-1) downto 0); --! blue input pixel
@@ -96,7 +104,7 @@ entity rgb2hsv is
 
     type mux_select_delay is array(0 to 4) of integer range 0 to 2;
     type max_delay is array(0 to 2) of integer range 0 to 2**wordsize;
-    
+
     constant c_60_degrees  : integer := integer(round(real( 60)/real(360) * real(2**wordsize)));
     constant c_120_degrees : integer := integer(round(real(120)/real(360) * real(2**wordsize)));
     constant c_240_degrees : integer := integer(round(real(240)/real(360) * real(2**wordsize)));
@@ -107,11 +115,11 @@ end entity;
 
 architecture behavioural of rgb2hsv is
 
-    
+
     -- signal declarations
-    signal rgdiff:                 integer range -2**wordsize to 2**wordsize; --! Difference between Red and Green input pixel 
-    signal brdiff:                 integer range -2**wordsize to 2**wordsize; --! Difference between Blue and Red input pixel 
-    signal gbdiff:                 integer range -2**wordsize to 2**wordsize; --! Difference between Green and Blue input pixel   
+    signal rgdiff:                 integer range -2**wordsize to 2**wordsize; --! Difference between Red and Green input pixel
+    signal brdiff:                 integer range -2**wordsize to 2**wordsize; --! Difference between Blue and Red input pixel
+    signal gbdiff:                 integer range -2**wordsize to 2**wordsize; --! Difference between Green and Blue input pixel
 
     signal c_rgdiff:               integer range -2**wordsize * c_60_degrees to 2**wordsize * c_60_degrees; --! rg_diff * 60 degrees
     signal c_brdiff:               integer range -2**wordsize * c_60_degrees to 2**wordsize * c_60_degrees; --! br_diff * 60 degrees
@@ -122,12 +130,12 @@ architecture behavioural of rgb2hsv is
     signal c_gbdiff_d0:            integer range -2**wordsize * c_60_degrees to 2**wordsize * c_60_degrees; --! delayed c_gbdiff
 
     signal c_rgdiff_div_max:       integer range -2**wordsize to 2**wordsize; --! delayed c_rgdiff divided by max
-    signal c_brdiff_div_max:       integer range -2**wordsize to 2**wordsize; --! delayed c_brdiff divided by max     
-    signal c_gbdiff_div_max:       integer range -2**wordsize to 2**wordsize; --! delayed c_gbdiff divided by max    
+    signal c_brdiff_div_max:       integer range -2**wordsize to 2**wordsize; --! delayed c_brdiff divided by max
+    signal c_gbdiff_div_max:       integer range -2**wordsize to 2**wordsize; --! delayed c_gbdiff divided by max
 
-    signal rg_mux_in:              integer range -2**wordsize to 2**wordsize;   
-    signal br_mux_in:              integer range -2**wordsize to 2**wordsize;      
-    signal gb_mux_in:              integer range -2**wordsize to 2**wordsize;     
+    signal rg_mux_in:              integer range -2**wordsize to 2**wordsize;
+    signal br_mux_in:              integer range -2**wordsize to 2**wordsize;
+    signal gb_mux_in:              integer range -2**wordsize to 2**wordsize;
 
     signal mux_select:             mux_select_delay;
 
@@ -141,7 +149,7 @@ architecture behavioural of rgb2hsv is
 
     signal max_min_min:            integer range 0 to 2**wordsize;
 
-    signal range_times_255:        integer range 0 to 2**wordsize * 255;     
+    signal range_times_255:        integer range 0 to 2**wordsize * 255;
     signal range_255_div_by_max:   integer range 0 to 2**wordsize;
 
     signal comp_max:               max_delay;
@@ -197,7 +205,7 @@ begin
         elsif rising_edge(clk) then
 
             if enable = '1' then
-                
+
                 red_i_int   := to_integer(unsigned(pixel_red_i));
                 green_i_int := to_integer(unsigned(pixel_green_i));
                 blue_i_int  := to_integer(unsigned(pixel_blue_i));
@@ -212,7 +220,7 @@ begin
                     r_versus_g_min <= red_i_int;
                     mux_select(0) <= 1;
                 end if;
-             
+
                 blue_pix_delay <= blue_i_int;
 
                 -- Second stage of comparison
@@ -276,6 +284,7 @@ begin
                 pixel_hue_o <= mux_out(wordsize-1 downto 0);
 
                 -- Saturation calculation
+                -- TODO: replace for shift?
                 range_times_255 <= max_min_min * 255;
 
                 if comp_max(1) /= 0 then
@@ -285,7 +294,7 @@ begin
                 end if;
                 sat_out := std_logic_vector(to_unsigned(range_255_div_by_max, wordsize));
                 pixel_sat_o <= sat_out;
- 
+
                 -- Value calculation
                 pixel_val_o <= std_logic_vector(to_unsigned(comp_max(2), wordsize));
 
